4e29a35dd8b1567458c7511ae388c9001dc01682
Prashil Gajjar <prashilgajjar7@gmail.com>
2021-06-20 14:15:45 +0530

    Day-5

---
+++ THA 5/style.js
@@ -0,0 +1,24 @@
+var addEventListner;
+let display = document.getElementById('display');
+let displayValue=''
+buttons = document.querySelectorAll('button');
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText = e.target.innerText;
+        console.log('button text=',buttonText);
+         if(buttonText=="C"){
+            displayValue =' ';
+            display.value=displayValue;
+        }
+        else if(buttonText=="="){
+            display.value=eval(displayValue)
+        }
+        else{
+            displayValue += buttonText;
+            display.value = displayValue;
+        }
+    }
+    )
+    
+}
+
